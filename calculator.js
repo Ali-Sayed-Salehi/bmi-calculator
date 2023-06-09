2398dc84276658fe002c289e9a9dcfc3ef54579d
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2023-06-09 00:54:22 -0400

    add calculation logic and for styling

--- calculator.js
+++ calculator.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-  res.send("hello")
+  res.sendFile(__dirname + "/bmi-calculator.html");
+});
+
+app.post("/bmi-calculator", (req, res) => {
+  let weight = req.body.weight;
+  let height = (req.body.height)/100;
+  let bmi = Math.round(weight / (height * height));
+  res.send(`Your BMI is ${bmi}`);
 });
 
 app.listen(port, () => {
